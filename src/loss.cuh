b77ab4ee855c000bcc7ec3d386a9df1de09b063e
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-30 17:17:32 -0500

    mae

--- src/loss.cuh
+++ src/loss.cuh
@@ -1,6 +1,6 @@
 #include <cuda_runtime.h>
 
-__global__ void meanSquaredError(float *y, float *y_pred, float *error, int n)
+__global__ void squaredErrorKernel(float *y, float *y_pred, float *error, int n)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -8,5 +8,15 @@ __global__ void meanSquaredError(float *y, float *y_pred, float *error, int n)
     {
         float diff = y[idx] - y_pred[idx];
         error[idx] = diff * diff;
+    }
+}
+
+__global__ void absoluteErrorKernel(float *y, float *y_pred, float *error, int n)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < n)
+    {
+        error[idx] = fabsf(y[idx] - y_pred[idx]);
     }
 }
