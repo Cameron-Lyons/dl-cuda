033f185481c886e63d919b3be1eeacba985c786f
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-30 17:37:55 -0500

    mcc kernel

--- src/metrics.cuh
+++ src/metrics.cuh
@@ -35,3 +35,16 @@ __global__ void f1Kernel(int *y, int *y_pred, int *TP, int *FP, int *FN, int n)
         FN[idx] = (y[idx] == 1 && y_pred[idx] == 0) ? 1 : 0;
     }
 }
+
+__global__ void mccKernel(int *y, int *y_pred, int *TP, int *FP, int *FN, int *TN, int n)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < n)
+    {
+        TP[idx] = (y[idx] == 1 && y_pred[idx] == 1) ? 1 : 0;
+        FP[idx] = (y[idx] == 0 && y_pred[idx] == 1) ? 1 : 0;
+        FN[idx] = (y[idx] == 1 && y_pred[idx] == 0) ? 1 : 0;
+        TN[idx] = (y[idx] == 0 && y_pred[idx] == 0) ? 1 : 0;
+    }
+}
