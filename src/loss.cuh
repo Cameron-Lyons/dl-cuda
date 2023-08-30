fafdd734a657ee0f55e38ca3d0734341889c3d60
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-30 17:22:14 -0500

    multiclass

--- src/loss.cuh
+++ src/loss.cuh
@@ -30,3 +30,19 @@ __global__ void binaryCrossEntropyKernel(float *y, float *y_pred, float *error, 
         error[idx] = -y[idx] * logf(y_pred[idx]) - (1.0f - y[idx]) * logf(1.0f - y_pred[idx]);
     }
 }
+
+__global__ void categoricalCrossEntropyKernel(float *y, float *y_pred, float *error, int n, int classes)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < n)
+    {
+        float loss = 0.0f;
+        for (int c = 0; c < classes; c++)
+        {
+            int offset = idx * classes + c;
+            loss -= y[offset] * logf(y_pred[offset] + 1e-10); // Added epsilon to avoid log(0)
+        }
+        error[idx] = loss;
+    }
+}
