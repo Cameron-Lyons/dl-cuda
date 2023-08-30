dc6703bcd72a0060e6817230c5d3220a27009cdc
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-30 17:20:16 -0500

    binary ce

--- src/loss.cuh
+++ src/loss.cuh
@@ -20,3 +20,13 @@ __global__ void absoluteErrorKernel(float *y, float *y_pred, float *error, int n
         error[idx] = fabsf(y[idx] - y_pred[idx]);
     }
 }
+
+__global__ void binaryCrossEntropyKernel(float *y, float *y_pred, float *error, int n)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < n)
+    {
+        error[idx] = -y[idx] * logf(y_pred[idx]) - (1.0f - y[idx]) * logf(1.0f - y_pred[idx]);
+    }
+}
