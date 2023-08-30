3ce75c468282b0b77462b924162dfd6f1ee48ff3
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-30 17:39:34 -0500

    compute

--- src/metrics.cuh
+++ src/metrics.cuh
@@ -34,6 +34,9 @@ __global__ void f1Kernel(int *y, int *y_pred, int *TP, int *FP, int *FN, int n)
         FP[idx] = (y[idx] == 0 && y_pred[idx] == 1) ? 1 : 0;
         FN[idx] = (y[idx] == 1 && y_pred[idx] == 0) ? 1 : 0;
     }
+    float precision = float(sum_TP) / (sum_TP + sum_FP);
+    float recall = float(sum_TP) / (sum_TP + sum_FN);
+    return 2.0 * (precision * recall) / (precision + recall);
 }
 
 __global__ void mccKernel(int *y, int *y_pred, int *TP, int *FP, int *FN, int *TN, int n)
@@ -47,4 +50,7 @@ __global__ void mccKernel(int *y, int *y_pred, int *TP, int *FP, int *FN, int *T
         FN[idx] = (y[idx] == 1 && y_pred[idx] == 0) ? 1 : 0;
         TN[idx] = (y[idx] == 0 && y_pred[idx] == 0) ? 1 : 0;
     }
+    float numerator = sum_TP * sum_TN - sum_FP * sum_FN;
+    float denominator = sqrt((sum_TP + FP) * (sum_TP + FN) * (sum_TN + FP) * (sum_TN + FN));
+    return (denominator == 0) ? 0 : numerator / denominator;
 }
