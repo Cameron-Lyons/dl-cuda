d41d69a2399b191565454178fbc5edaa4e28d7af
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-30 14:23:49 -0500

    sigmoid

--- src/activation.cuh
+++ src/activation.cuh
@@ -1,8 +1,18 @@
-__global__ void tanhKernel(float *data, int n)
+__global__ void tanh(float *data, int n)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < n)
     {
         data[idx] = tanhf(data[idx]);
+    }
+}
+
+__global__ void sigmoid(float *input, float *output, int n)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < n)
+    {
+        output[idx] = 1.0f / (1.0f + expf(-input[idx]));
     }
 }
