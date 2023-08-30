f585e8ac80ce767e8e6fd6eac2577979fd4a5ffb
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-30 18:04:00 -0500

    elman rnn

--- src/layers.cuh
+++ src/layers.cuh
@@ -1,40 +1,58 @@
 #include <cuda_runtime.h>
 
-__global__ void linearLayerKernel(float *X, float *W, float *b, float *Y, int n, int in_features, int out_features)
-{
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void linearLayerKernel(float *X, float *W, float *b, float *Y, int n,
+                                  int in_features, int out_features) {
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (row < n && col < out_features)
-    {
-        float sum = 0.0f;
-        for (int i = 0; i < in_features; i++)
-        {
-            sum += X[row * in_features + i] * W[i * out_features + col];
-        }
-        Y[row * out_features + col] = sum + b[col];
+  if (row < n && col < out_features) {
+    float sum = 0.0f;
+    for (int i = 0; i < in_features; i++) {
+      sum += X[row * in_features + i] * W[i * out_features + col];
     }
+    Y[row * out_features + col] = sum + b[col];
+  }
 }
 
-__global__ void lstmKernel(float *x, float *h_prev, float *c_prev,
-                           float *Wf, float *Wi, float *Wc, float *Wo,
-                           float *bf, float *bi, float *bc, float *bo,
-                           float *h, float *c, int sequence_length)
-{
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void lstmKernel(float *x, float *h_prev, float *c_prev, float *Wf,
+                           float *Wi, float *Wc, float *Wo, float *bf,
+                           float *bi, float *bc, float *bo, float *h, float *c,
+                           int sequence_length) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (idx < sequence_length)
-    {
-        // Forget gate
-        float ft = sigmoid(dot(Wf, concat(h_prev, x[idx])) + bf);
-        // Input gate
-        float it = sigmoid(dot(Wi, concat(h_prev, x[idx])) + bi);
-        // Cell state
-        float c_tilde = tanh(dot(Wc, concat(h_prev, x[idx])) + bc);
-        c[idx] = ft * c_prev[idx] + it * c_tilde;
-        // Output gate
-        float ot = sigmoid(dot(Wo, concat(h_prev, x[idx])) + bo);
-        // Hidden state
-        h[idx] = ot * tanh(c[idx]);
+  if (idx < sequence_length) {
+    // Forget gate
+    float ft = sigmoid(dot(Wf, concat(h_prev, x[idx])) + bf);
+    // Input gate
+    float it = sigmoid(dot(Wi, concat(h_prev, x[idx])) + bi);
+    // Cell state
+    float c_tilde = tanh(dot(Wc, concat(h_prev, x[idx])) + bc);
+    c[idx] = ft * c_prev[idx] + it * c_tilde;
+    // Output gate
+    float ot = sigmoid(dot(Wo, concat(h_prev, x[idx])) + bo);
+    // Hidden state
+    h[idx] = ot * tanh(c[idx]);
+  }
+}
+
+__global__ void elmanRnnKernel(float *x, float *h_prev, float *Wxh, float *Whh,
+                               float *b_h, float *Why, float *b_y, float *h,
+                               float *y, int sequence_length) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (idx < sequence_length) {
+    // Compute hidden state
+    float hidden_sum = 0.0;
+    for (int i = 0; i < HIDDEN_SIZE; i++) {
+      hidden_sum += Wxh[idx * HIDDEN_SIZE + i] * x[idx] + Whh[i] * h_prev[idx];
     }
+    h[idx] = tanhf(hidden_sum + b_h[idx]);
+
+    // Compute output
+    float output_sum = 0.0;
+    for (int i = 0; i < OUTPUT_SIZE; i++) {
+      output_sum += Why[idx * OUTPUT_SIZE + i] * h[idx];
+    }
+    y[idx] = output_sum + b_y[idx];
+  }
 }
