401ad94ea1c93e51174af7048c273f52f5593d81
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-30 14:26:18 -0500

    relu

--- src/activation.cuh
+++ src/activation.cuh
@@ -16,3 +16,13 @@ __global__ void sigmoid(float *input, float *output, int n)
         output[idx] = 1.0f / (1.0f + expf(-input[idx]));
     }
 }
+
+__global__ void relu(float *input, float *output, int n)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < n)
+    {
+        output[idx] = (input[idx] > 0.0f) ? input[idx] : 0.0f;
+    }
+}
