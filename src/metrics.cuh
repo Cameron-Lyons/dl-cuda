3fa6f80fc5a29865c19b22c49798213152185e12
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-30 17:31:53 -0500

    accuracy

--- src/metrics.cuh
+++ src/metrics.cuh
@@ -13,3 +13,13 @@ __global__ void r2Kernel(float *y, float *y_pred, float *numerator, float *denom
         denominator[idx] = diff_mean * diff_mean;
     }
 }
+
+__global__ void accuracyKernel(int *y, int *y_pred, int *correct_preds, int n)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < n)
+    {
+        correct_preds[idx] = (y[idx] == y_pred[idx]) ? 1 : 0;
+    }
+}
