d82d8086cbb13f5dce8816c4ea4fb195d718c3ee
Cameron Lyons <cameron.lyons2@gmail.com>
2023-09-01 08:55:39 -0500

    layer norm

--- src/transformer.cuh
+++ src/transformer.cuh
@@ -35,3 +35,44 @@ void scaled_dot_product_attention(float *output, const float *queries,
 
   cudaDeviceSynchronize();
 }
+
+__global__ void layer_norm_kernel(float *output, const float *input,
+                                  const float *gamma, const float *beta,
+                                  int feature_size, float epsilon) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (i < feature_size) {
+    float mean = 0.0;
+    float var = 0.0;
+    for (int d = 0; d < feature_size; d++) {
+      mean += input[i * feature_size + d];
+    }
+    mean /= feature_size;
+
+    for (int d = 0; d < feature_size; d++) {
+      var += (input[i * feature_size + d] - mean) *
+             (input[i * feature_size + d] - mean);
+    }
+    var /= feature_size;
+
+    // Normalize and scale/shift
+    for (int d = 0; d < feature_size; d++) {
+      output[i * feature_size + d] = gamma[d] *
+                                         (input[i * feature_size + d] - mean) /
+                                         sqrtf(var + epsilon) +
+                                     beta[d];
+    }
+  }
+}
+
+void layer_normalization(float *output, const float *input, const float *gamma,
+                         const float *beta, int batch_size, int feature_size,
+                         float epsilon = 1e-5) {
+  dim3 threadsPerBlock(256);
+  dim3 numBlocks((batch_size + threadsPerBlock.x - 1) / threadsPerBlock.x);
+
+  layer_norm_kernel<<<numBlocks, threadsPerBlock>>>(output, input, gamma, beta,
+                                                    feature_size, epsilon);
+
+  cudaDeviceSynchronize();
+}
