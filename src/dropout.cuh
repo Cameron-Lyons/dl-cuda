58c4fc7c0c95f4c92e5c90243d78a517f70957fe
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-31 19:07:52 -0500

    better rng

--- src/dropout.cuh
+++ src/dropout.cuh
@@ -1,9 +1,20 @@
+#include <curand_kernel.h>
+
+const int THREADS_PER_BLOCK = 256;
+
+__global__ void initializeCurandStates(curandState_t *states,
+                                       unsigned long seed) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  curand_init(seed, idx, 0, &states[idx]);
+}
+
 __global__ void dropoutKernel(float *input, float *output, float *mask,
-                              float dropout_prob, int size) {
+                              float dropout_prob, int size,
+                              curandState_t *states) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
   if (idx < size) {
-    float rand_val = (float)(idx * 1234) / INT_MAX;
+    float rand_val = curand_uniform(&states[idx]);
 
     mask[idx] = (rand_val > dropout_prob) ? 1.0f : 0.0f;
 
