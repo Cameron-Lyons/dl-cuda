176894abbee087886fc4e97116dfd9cef40deba7
Cameron <cameron.lyons2@gmail.com>
2023-09-15 19:59:54 -0500

    move read to header

--- src/main.cu
+++ src/main.cu
@@ -1,29 +1,11 @@
 #include "layers.cuh"
+#include "read_csv.h"
 #include "sequential.cuh"
 #include "transformer.cuh"
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <vector>
-
-void loadCSV(const std::string &filename, std::vector<float *> &data) {
-  std::ifstream file(filename);
-  std::string line;
-
-  while (std::getline(file, line)) {
-    std::stringstream ss(line);
-    std::string item;
-    std::vector<float> row;
-
-    while (std::getline(ss, item, ',')) {
-      row.push_back(std::stof(item));
-    }
-
-    float *rowData = new float[row.size()];
-    std::copy(row.begin(), row.end(), rowData);
-    data.push_back(rowData);
-  }
-}
 
 int main() {
   std::vector<float *> data;
