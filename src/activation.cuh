81e2d3b31320be998ce914e13b89047ac88542f5
Cameron <cameron.lyons2@gmail.com>
2023-09-19 22:26:54 -0500

    thread coarsen

--- src/activation.cuh
+++ src/activation.cuh
@@ -1,9 +1,10 @@
 #include <cuda_runtime.h>
 
-__global__ void tanh(float *data, int n) {
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (idx < n) {
-    data[idx] = tanhf(data[idx]);
+__global__ void tanh(float *data, int n, int elements_per_thread) {
+  int idx = (blockIdx.x * blockDim.x + threadIdx.x) * elements_per_thread;
+
+  for (int i = 0; i < elements_per_thread && (idx + i) < n; i++) {
+    data[idx + i] = tanhf(data[idx + i]);
   }
 }
 
